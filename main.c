79040dab6e9b8d03a05dee35583532a02c422db7
bastientt <bastienschneider45@gmail.com>
2024-03-12 16:08:13 +0100

    fix : remove the (null)

--- main.c
+++ main.c
@@ -8,12 +8,20 @@ int main(int argc, char ** argv) {
     Facts* lstF = malloc(sizeof(Facts*));
     lstF->name=malloc(20*sizeof(char));
     strcpy(lstF->name,"miam");
+    lstF->next=NULL;
+
+    Facts* lstF2 = malloc(sizeof(Facts*));
+    lstF2->name=malloc(20*sizeof(char));
+    strcpy(lstF2->name,"leramadan");
+    lstF2->next=NULL;
 
     Rules* lstR = initRules();
     Rules* elm=malloc(sizeof(Rules));
     elm->name=malloc(20*sizeof(char));
     elm->factList=malloc(sizeof(Facts*));
+  elm->factList=lstF;
     strcpy(elm->name,"bite");
+
 
 
    char * testLecture= malloc(400*sizeof(char));
@@ -22,6 +30,7 @@ int main(int argc, char ** argv) {
     printf("\n");
     readInv(testLecture);*/
     elm->factList= addFact(elm->factList,lstF);
+    elm->factList= addFact(elm->factList,lstF2);
     lstR= addRules(lstR,elm);
     showRules(lstR);
 
