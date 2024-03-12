e895fc2266be744fd01ff9a5073d6e80ed1d6cbc
Maxou <Jackson.Kyoshi@gmail.com>
2024-03-12 11:14:40 +0100

    feat: Struct

--- func.h
+++ func.h
@@ -2,3 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+
+typedef struct Facts{
+    char * name;
+    Facts * next;
+}Facts;
+
+typedef struct Rules{
+    char * name;
+    Rules * next;
+    struct Facts * factList;
+}Rules;
